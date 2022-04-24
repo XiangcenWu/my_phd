c30a1fb775e2f7a9a1093284d624c42ba9523931
XiangcenWu <wxiangcen@gmail.com>
2022-04-23 19:31:13 -0700

    tgdfs

--- 强化学习/openai_gym/CartPole.py
+++ 强化学习/openai_gym/CartPole.py
@@ -1,12 +1,6 @@
 import gym
-import time
 
-# init the gym environment
-e = gym.make('CartPole-v0')
-# There are four class members inside this class
-# action_space, observation_space, reset(), step() and render()
-# reset the env, and return the observation 
-print(e.action_space)
-print(e.observation_space)
 
+def random_cartpole():
+    print('This is the code to create a random cartpole agent')
 
--- 强化学习/openai_gym/readme.md
+++ 强化学习/openai_gym/readme.md
@@ -10,7 +10,12 @@ conda install -c conda-forge gym-all
 ## Space Class
 不管是agent的行为（action），亦或是agent的观测（observation），他们都可以用一个张量表示，gym把这类张量写在了一个叫做Sample的类里，这其中有两个继承了Sample的子类最为重要。
 
+#### Space.sample()
+在空间内随机抽样
+#### Space.contains(x)
+查看x是否再空间中
 ### Discrete
-
+Discrete类里面储存了n个互斥的数。每一次只能sample其中一个数。
 
 ### Box
+储存了一个n维张量，定义了最大数，最小数以及张量的shape。
